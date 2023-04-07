b6f7ea2585db98d822e3c121e6b19a56546c2cbe
uyyii1 <50738657+uyyii1@users.noreply.github.com>
2023-04-07 19:11:37 +0800

    undo firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import 'firebase/auth'
-import 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCiPDi06BbLS529ReuinPW5S1ZIStNsCHo",
@@ -12,53 +10,6 @@ const firebaseConfig = {
     measurementId: "G-WLZ780XPRX"
 };
 
-const firebase = initializeApp(firebaseConfig)
-
-const auth = firebase.auth()
-
-export function useAuth() {
-    const user = ref(null)
-    const unsubscribe = auth.onStateChanged(user => (user.value = _user))
-    onUnmounted(unsubscribe)
-    const isLogin = computed(() => user.value != null)
-
-    const signIn = async () => {
-        const googleProvider = new firebase.authGoogleAuthProvider()
-        await auth.signInWithPopup(googleProvider)
-    }
-    const signOut = () => auth.signOut()
-
-    return{user, isLogin, signIn, signOut}
-}
-
-const firestore = firebase.firestore()
-const messagesCollection = firestore.collection('messages')
-const messageQuery = messagesCollection.orderBy('createdAt', 'desc').limit(100)
-
-export function useChat() {
-    const messages = ref([])
-    const unsubscribe = messageQuery.onSnapshot(snapshot => {
-        messages.value = snapshot.docs
-        .map(doc => ({id: doc.id, ...doc.data()}))
-        .reverse()
-    })
-    onUnmounted(unsubscribe)
-
-    const { user, isLogin } = useAuth()
-    const sendMessage = text => {
-        if (!isLogin.value) return
-        const { photoURL, uid, displayName } = user.value
-        messagesCollection.add({
-            userName: displayName,
-            userId: uid,
-            userPhotoURL: photoURL,
-            text: text,
-            createdAt: firebase.firestore.FieldValue.serverTimeStamp()
-        })
-    }
-
-    return { messages, sendMessage }
-}
 const app = initializeApp(firebaseConfig);
 
 export default app;
