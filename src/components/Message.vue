24583e91feb638482352f2f6d6ad18509f013dc7
uyyii1 <50738657+uyyii1@users.noreply.github.com>
2023-04-07 19:07:35 +0800

    chat and message components

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class = "message">
+        <span v-if="!sender"> {{ name }}</span>
+        <div class = "flex" :class = "sender ? 'flex-row-reverse' : '' ">
+            <Avatar class = "mt-1" :src="photoURL" />
+            <div class="text w-3/4" :class = "sender ? 'bg-green-800' : 'bg-gray-700'">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Avatar } from './Avatar.vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
